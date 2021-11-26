9025c6bf1468102c8dfbda9cf6b89844cbeea05f
Nilay Nath Sharan <92233871+SubstantialCattle5@users.noreply.github.com>
2021-11-26 16:39:31 +0530

    uploading the code

---
+++ main.py
@@ -0,0 +1,106 @@
+import smtplib
+import datetime as dt
+import random as rd
+import tkinter as tk
+from tkinter import messagebox
+
+# Screen size
+WIDTH, HEIGHT = 800, 650
+
+# Colours
+WHITE = '#F0F5F9'
+DARK_WHITE = '#C9D6DF'
+GREY = '#52616B'
+BLACK = '#1E2022'
+
+# test email info
+myemail = '************'
+mypassword = '***********'
+send, subject, body = str(), str(), str()
+
+
+def send():
+    # Sender's email , subject  and body
+    sender = sender_email.get().strip()
+    subject = sub.get()
+    body = body_text.get(1.0, "end-1c")
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        # Encrypting the mail contents
+        connection.starttls()
+        connection.login(user=myemail, password=mypassword)
+        # sending the mail
+        connection.sendmail(from_addr=myemail,
+                            to_addrs=f'{sender}',
+                            msg=f'Subject : {subject} \n\n {body} ')
+        messagebox.showinfo(
+            title='Congrats!',
+            message='The email is sent!'
+        )
+
+
+# Window
+root = tk.Tk()
+root.minsize(width=WIDTH, height=HEIGHT)
+root.title('Email')
+root.config(bg=DARK_WHITE)
+
+# logo
+logo = tk.Canvas(master=root, width=350, height=210, bg=DARK_WHITE, highlightthickness=0)
+img = tk.PhotoImage(file="img.png")
+logo.create_image(120, 90, image=img)
+logo.grid(row=1, column=2, columnspan=2)
+
+# Email heading
+email_label = tk.Label(master=root, text=f'Email Id : {myemail} ', bg=DARK_WHITE, fg=BLACK, font=(24))
+email_label.grid(row=2, column=1, columnspan=2, sticky='w')
+
+# Space
+space = tk.Label(master=root, text=' ', bg=DARK_WHITE)
+space.grid(row=3, column=2)
+
+# To label (sender)
+To_label = tk.Label(master=root, text='To :', bg=DARK_WHITE, fg=BLACK, font=(24))
+To_label.grid(row=4, column=1, sticky='w')
+
+#  Email id entry(sender)
+sender_email = tk.Entry(master=root, width=60, bg=WHITE, relief=tk.SUNKEN, font=('helvetica', 14))
+sender_email.grid(row=4, column=2, columnspan=3, ipady=3, sticky='w')
+sender_email.insert(index=0, string='nilaynathsharan16@gmail.com')
+
+# Space
+space = tk.Label(master=root, text=' ', bg=DARK_WHITE, font=('helvetica', 14))
+space.grid(row=5, column=2)
+
+#  Subject
+#  Subject Label
+sub_label = tk.Label(master=root, text='Subject :', bg=DARK_WHITE, fg=BLACK, font=(24))
+sub_label.grid(row=6, column=1)
+
+#  Subject Entry
+sub = tk.Entry(master=root, width=60, bg=WHITE, relief=tk.SUNKEN, font=('helvetica', 14))
+sub.grid(row=6, column=2, columnspan=3, sticky='w', ipady=3)
+
+# Space
+space = tk.Label(master=root, text=' ', bg=DARK_WHITE)
+space.grid(row=7, column=2)
+
+# Body
+# Body label
+body_label = tk.Label(master=root, text='Body :', bg=DARK_WHITE, fg=BLACK, font=(24))
+body_label.grid(row=8, column=1, sticky='nw')
+
+# Body Text
+body_text = tk.Text(master=root, bg=WHITE, width=60, height=10, font=('helvetica', 14), relief=tk.SUNKEN)
+body_text.grid(row=8, column=2, columnspan=2)
+
+# Space
+space = tk.Label(master=root, text=' ', bg=DARK_WHITE)
+space.grid(row=9, column=2)
+
+# send button
+
+send_button = tk.Button(master=root, text='Send', bg=GREY, fg=DARK_WHITE, activebackground=BLACK,
+                        activeforeground=WHITE, width=20, command=send)
+send_button.grid(row=10, column=3, sticky='w')
+
+root.mainloop()
